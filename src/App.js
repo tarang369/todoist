bbd597c2109c53491f8c253fb04ffb4b82cbaae1
tdev.js <tdev.js@gmail.com>
2020-08-02 01:07:27 +0530

    completed GET data from firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import './App.css';
+import { Header } from './components/layout/Header';
+import { Content } from './components/layout/Content';
 
-function App() {
-  return <div className='App'></div>;
-}
-
-export default App;
+export const App = () => (
+  <div className='App'>
+    <Header />
+    <Content />
+  </div>
+);
