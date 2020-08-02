a1aa92cfc8001061f6691902753c994dccc4bff9
tdev.js <tdev.js@gmail.com>
2020-08-02 23:51:43 +0530

    add styles and ui update

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import { FaPizzaSlice } from 'react-icons/fa';
+import React from "react";
+import { FaPizzaSlice } from "react-icons/fa";
 export const Header = () => {
   return (
-    <header className='header' data-test-id='header'>
+    <header className="header" data-test-id="header">
       <nav>
-        <div className='logo'>
-          <img src='/images/logo.png' alt='Todoist' />
+        <div className="logo">
+          <img src="/images/logo.png" alt="Todoist" />
         </div>
-        <div className='settings'>
+        <div className="settings">
           <ul>
-            <li>+</li>
-            <li>
+            <li data-test-id="quick-add-task-action" className="settings__add">
+              +
+            </li>
+            <li data-test-id="dark-mode-action" className="settings__darkmode">
               <FaPizzaSlice />
             </li>
           </ul>
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
   FaChevronDown,
   FaInbox,
   FaRegCalendarAlt,
   FaRegCalendar,
-} from 'react-icons/fa';
+} from "react-icons/fa";
+import { useSelectedProjectValue } from "../../context";
+import { Projects } from "../Projects";
 export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue();
+  const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
   return (
-    <div className='sidebar' data-testid='sidebar'>
-      <ul className='sidebar__generic'>
-        <li>
+    <div className="sidebar" data-testid="sidebar">
+      <ul className="sidebar__generic">
+        <li data-test-id="inbox" className="inbox">
           <span>
             <FaInbox />
           </span>
           <span>Inbox</span>
         </li>
-        <li>
+        <li data-test-id="today" className="today">
           <span>
             <FaRegCalendar />
           </span>
           <span>Today</span>
         </li>
-        <li>
+        <li data-test-id="next_7" className="next_7">
           <span>
             <FaRegCalendarAlt />
           </span>
           <span>Next 7 days</span>
         </li>
       </ul>
-      <div className='sidebar__middle'>
+      <div className="sidebar__middle">
         <span>
           <FaChevronDown />
         </span>
         <h2>Projects</h2>
       </div>
-      <ul className='sidebar__projects'>Projects will be here!</ul>
-      Add Project Component here!
+      <ul className="sidebar__projects">{showProjects && <Projects />}</ul>
     </div>
   );
 };
